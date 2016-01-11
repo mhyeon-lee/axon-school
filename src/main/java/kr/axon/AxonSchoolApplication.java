093ba9e38ca6f5cfabf5551798d46a23a479fa44
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-11 21:55:57 +0900

    Create axon-school project.

---
+++ src/main/java/kr/axon/AxonSchoolApplication.java
@@ -0,0 +1,12 @@
+package kr.axon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonSchoolApplication.class, args);
+    }
+}
