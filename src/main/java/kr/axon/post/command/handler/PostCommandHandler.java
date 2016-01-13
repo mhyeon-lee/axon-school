507c9b5ae7c1cc1dfc2d5ff8b9b5436f9f1e3330
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-13 22:03:58 +0900

    Change name repository to eventSourcingRepository.

--- src/main/java/kr/axon/post/command/handler/PostCommandHandler.java
+++ src/main/java/kr/axon/post/command/handler/PostCommandHandler.java
@@ -11,28 +11,28 @@ import static kr.axon.post.command.api.PostCommand.*;
 @Component
 public class PostCommandHandler {
 
-    private final Repository<Post> repository;
+    private final Repository<Post> eventSourcingRepository;
 
     @Autowired
-    public PostCommandHandler(Repository repository) {
-        this.repository = repository;
+    public PostCommandHandler(Repository eventSourcingRepository) {
+        this.eventSourcingRepository = eventSourcingRepository;
     }
 
     @CommandHandler
     public void handler(CreatePostCommand command) {
         Post post = new Post(command);
-        repository.add(post);
+        eventSourcingRepository.add(post);
     }
 
     @CommandHandler
     public void handle(ModifyPostCommand command) {
-        Post post = repository.load(command.getId());
+        Post post = eventSourcingRepository.load(command.getId());
         post.modify(command);
     }
 
     @CommandHandler
     public void handle(DeletePostCommand command) {
-        Post post = repository.load(command.getId());
+        Post post = eventSourcingRepository.load(command.getId());
         if (!post.isDeleted()) {
             post.delete(command);
         }
