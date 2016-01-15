679549ee9fc7dd164d1fe2bdb03853c7d2296145
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-15 22:38:25 +0900

    Arrange import.

--- src/test/java/kr/axon/post/PostIntegrationTest.java
+++ src/test/java/kr/axon/post/PostIntegrationTest.java
@@ -11,7 +11,6 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
