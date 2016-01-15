943458ee236286856a313aea3929a3833b4b64d7
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-15 11:19:06 +0900

    Remove duplicated @WebAppConfiguration

--- src/test/java/kr/axon/post/PostIntegrationTest.java
+++ src/test/java/kr/axon/post/PostIntegrationTest.java
@@ -11,9 +11,9 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static kr.axon.post.command.api.PostCommand.*;
@@ -22,7 +22,6 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AxonSchoolApplication.class)
-@WebAppConfiguration
 @Transactional
 public class PostIntegrationTest extends AbstractPostCommandEventFixture {
 
