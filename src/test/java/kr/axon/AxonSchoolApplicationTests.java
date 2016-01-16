ca5688e8774b983195f54f97d4f8e84fc607d3b0
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-16 18:55:56 +0900

    Remove not used autowired in test

--- src/test/java/kr/axon/AxonSchoolApplicationTests.java
+++ src/test/java/kr/axon/AxonSchoolApplicationTests.java
@@ -1,9 +1,7 @@
 package kr.axon;
 
-import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,9 +10,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringApplicationConfiguration(classes = AxonSchoolApplication.class)
 @WebAppConfiguration
 public class AxonSchoolApplicationTests {
-
-    @Autowired
-    private CommandGateway commandGateWay;
 
     @Test
     public void contextLoads() {
