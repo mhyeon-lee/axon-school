ece1de3558673b4760f1f48e977c37fe7c2f1909
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-17 23:42:28 +0900

    Remove not use parameter.

--- src/main/java/kr/axon/post/controller/PostRestControllerExceptionHandler.java
+++ src/main/java/kr/axon/post/controller/PostRestControllerExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -13,8 +12,7 @@ import java.io.IOException;
 public class PostRestControllerExceptionHandler {
 
     @ExceptionHandler(PostNotFoundException.class)
-    public void handlePostNotFoundException(HttpServletRequest request,
-                                            HttpServletResponse response,
+    public void handlePostNotFoundException(HttpServletResponse response,
                                             PostNotFoundException ex) throws IOException {
         response.sendError(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
