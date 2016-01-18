e3d046ac9efd8cc18802d93d422e42fb6de87685
mhyeon.lee <mhyeon.lee@navercorp.com>
2016-01-18 10:06:19 +0900

    Set @Transactional(readOnly = true) on repository.

--- src/main/java/kr/axon/post/query/repository/PostQueryRepository.java
+++ src/main/java/kr/axon/post/query/repository/PostQueryRepository.java
@@ -3,9 +3,11 @@ package kr.axon.post.query.repository;
 import kr.axon.post.command.domain.PostIdentifier;
 import kr.axon.post.query.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface PostQueryRepository extends JpaRepository<Post, PostIdentifier> {
 
     Optional<Post> findById(PostIdentifier id);
